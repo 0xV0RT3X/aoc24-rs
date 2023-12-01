075e8b93d65add656cbca0ffb644ee60507e92a3
0xV0RT3X <eric.reps@gmail.com>
2023-12-01 11:36:25 +0100

    refactor day_01

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,18 +2,20 @@ fn extract_digits(line: &str) -> (u32, u32) {
     let mut p1_digits = Vec::new();
     let mut p2_digits = Vec::new();
 
-    for (i, c) in line.chars().enumerate() {
-        if c.is_digit(10) {
-            p1_digits.push(c);
-            p2_digits.push(c);
+    for (line_char_idx, line_char) in line.chars().enumerate() {
+        if char::is_digit(line_char, 10) {
+            p1_digits.push(line_char);
+            p2_digits.push(line_char);
         }
 
-        for (d, val) in ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-            .iter()
-            .enumerate()
+        for (spelled_digit_idx, spelled_digit) in [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ]
+        .iter()
+        .enumerate()
         {
-            if line[i..].starts_with(val) {
-                p2_digits.push((d + 1).to_string().parse().unwrap());
+            if line[line_char_idx..].starts_with(spelled_digit) {
+                p2_digits.push((spelled_digit_idx + 1).to_string().parse().unwrap());
             }
         }
     }
@@ -38,10 +40,13 @@ fn main() {
     let mut part_2_sum = 0;
 
     for line in lines {
-        let (current_p1, current_p2) = extract_digits(line);
-        part_1_sum += current_p1;
-        part_2_sum += current_p2;
+        let (part_1_number, part_2_number) = extract_digits(line);
+        part_1_sum += part_1_number;
+        part_2_sum += part_2_number;
     }
 
-    println!("Part I: {}\nPart II: {}", part_1_sum, part_2_sum);
+    assert_eq!(part_1_sum, 54_916);
+    assert_eq!(part_2_sum, 54_728);
+
+    println!("Part 1: {}\nPart 2: {}", part_1_sum, part_2_sum);
 }
